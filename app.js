882e28ff449f2b7e6bf4c037f8285a4bc57e1300
Jeff So <jeffso9820@gmail.com>
2020-09-05 13:19:17 -0400

    update on app.js to include additional get routes

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const ejs = require("ejs");
 
 const app = express();
 
 const randomtext = "In on announcing if of comparison pianoforte projection. Maids hoped gay yet bed asked blind dried point. On abroad danger likely regret twenty edward do. Too horrible consider followed may differed age. An rest if more five mr of. Age just her rank met down way. Attended required so in cheerful an. Domestic replying she resolved him for did. Rather in lasted no within no."
+const aboutText =  "In on announcing if of comparison pianoforte projection. Maids hoped gay yet bed asked blind dried point. On abroad danger likely regret twenty edward do. Too horrible consider followed may differed age. An rest if more five mr of. Age just her rank met down way. Attended required so in cheerful an. Domestic replying she resolved him for did. Rather in lasted no within no."
+const contactText = "In on announcing if of comparison pianoforte projection. Maids hoped gay yet bed asked blind dried point. On abroad danger likely regret twenty edward do. Too horrible consider followed may differed age. An rest if more five mr of. Age just her rank met down way. Attended required so in cheerful an. Domestic replying she resolved him for did. Rather in lasted no within no."
+
+app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-app.get("/", function(req, res){
-  res.render("home", {homeStartingContent: randomtext});
+
+app.get("/", function(req, res) {
+  res.render("home", {
+    homeStartingContent: randomtext
+  });
+});
+
+app.get("/about", function(req, res) {
+  res.render("about", {
+    aboutStartingContent: aboutText
+  });
+});
+
+app.get("/contact", function(req, res) {
+  res.render("contact", {
+    contactStartingContent: contactText
+  });
 });
 
 
